e4fb92a385b91b4d83dcdcc782f75b8cde67a57b
nitsan <nitsanbh@gmail.com>
2024-02-18 16:21:15 +0200

    added emscripten macros

--- engine/search.cpp
+++ engine/search.cpp
@@ -28,7 +28,7 @@ std::optional<SearchResult> search(const std::string& pattern_iso_8859_8) {
 /** 
  * for wasm to export. 
  * result consists of 41 (least-significant) bits.
- * bit 0: is found (if it's 0 then the rest is invalid)
+ * bit 0: whether has result (if it's 0 then the rest is invalid)
  * bits 1-20: index in string
  * bits 21-40: step (diloog) size
  */
--- engine/search.hpp
+++ engine/search.hpp
@@ -10,11 +10,20 @@ struct SearchResult {
 // for internal ergonomic use
 std::optional<SearchResult> search(const std::string& pattern_iso_8859_8);
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+EMSCRIPTEN_KEEPALIVE
+#endif
+
+extern "C" {
+
 /** 
  * for wasm to export. 
  * result consists of 41 (least-significant) bits.
- * bit 0: is found (if it's 0 then the rest is invalid)
+ * bit 0: whether has result (if it's 0 then the rest is invalid)
  * bits 1-20: index in string
  * bits 21-40: step (diloog) size
  */
 uint64_t search__packed_result(const char *pattern_iso_8859_8);
+
+}
