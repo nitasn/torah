8cf25c2cc565d16a3a55c48969338cb4a8454f5a
nitsan <nitsanbh@gmail.com>
2024-02-19 11:46:05 +0200

    removed dead comment

--- engine/boyer-moore-els.hpp
+++ engine/boyer-moore-els.hpp
@@ -4,7 +4,6 @@
 #include <stddef.h> // for size_t
 
 uint8_t *boyer_moore_els(
-  // std::vector<uint8_t>
   uint8_t *string, size_t stringlen, 
   uint8_t *pat, size_t patlen, 
   size_t *ptr_step // a.k.a the "diloog"
