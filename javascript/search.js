3b6250470d2673b2aeb18241015b104614136924
nitsan <nitsanbh@gmail.com>
2024-02-19 20:12:17 +0200

    a little tider

--- javascript/search.js
+++ javascript/search.js
@@ -30,12 +30,13 @@ let _next_id = 0;
 
 /**
  * Equidistant Letter Sequence ("דילוגי אותיות") Search in the Torah, in Hebrew.
+ * Also known as "The Bible Code"
  * @param {string} pattern in Hebrew
- * @param {{ min_step: number | undefined, max_step: number | undefined }}
+ * @param {{ min_step: number?, max_step: number? }}
  * @returns {Promise<{index: number, step: number} | null>} `{ index, number }` or `null`
  */
-function search(pattern, { min_step = 0, max_step = 0 } = {}) {
-  if (!engineLoaded) throw new Error("engine is not ready");
+function search(pattern, { min_step = null, max_step = null } = {}) {
+  if (!engineLoaded) throw new Error("engine hasn't loaded yet");
   let resolvePromise;
   const promise = new Promise((resolve) => (resolvePromise = resolve));
   callbacksRegistery.set(_next_id, resolvePromise);
--- javascript/worker.js
+++ javascript/worker.js
@@ -8,19 +8,23 @@ Module.onRuntimeInitialized = () => {
 onmessage = ({ data }) => {
   const { search_id, pattern, min_step, max_step } = data;
 
-  // malloc 8 bytes for two int32 numbers
+  // malloc 8 bytes for two uint32 numbers
   const pIndex = Module._malloc(8);
   const pStep = pIndex + 4;
 
-  const did_find = c_style__search(pattern + '\0', min_step, max_step, pIndex, pStep);
+  const did_find = c_style__search(
+    pattern + '\0', 
+    +min_step || 0, 
+    +max_step || 0, 
+    pIndex, pStep);
 
   const [index, step] = new Int32Array(Module.HEAP32.buffer, pIndex, 2);
   Module._free(pIndex);
 
   postMessage({
     type: 'done',
-    result: did_find ? { index, step } : null,
     search_id,
+    result: did_find ? { index, step } : null,
   });
 };
 
